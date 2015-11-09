bbcc7bd503c26cfdc8478762513235c1472e11a0
weiweiji <weiweiji@yaomaiche.com>
2015-11-09 15:36:29 +0800

    u

--- src/views/about.vue
+++ src/views/about.vue
@@ -2,6 +2,10 @@
 <div>
   <p>about-{{msg}}</p>
   测试图片
+  <!-- <img src='{{imageSrc}}'> 绑定图片的时候使用 :src，如果直接用src 会有如下告警，会出现一个404请求-->
+  <!-- [Vue warn]: src="{{imageSrc}}": interpolation in "src" attribute will cause a 404 request. Use v-bind:src instead. -->
+
+  <img :src='imageSrc'>
   <div class="img_holder"></div>
   图片下的文字
  </div>
@@ -11,6 +15,7 @@
 module.exports = {
 	data:function(){
 		return {
+			imageSrc:require('../images/logo.png'),
 			msg:'aboutMessage',
 			title:'about'
 		}
